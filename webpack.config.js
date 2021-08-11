e0bd3027480e3947442ea7ae0fc0be5669297a86
Abhinav Raj <abhinav.raj0509@gmail.com>
2021-08-11 15:53:20 +0530

    initial web pack configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js",
+  },
+
+  devServer: {
+    port: 3010,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
